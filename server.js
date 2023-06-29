b96d1af53cc9d0722ad54934d62255e47cd23100
wangeline <137566286+wangeline@users.noreply.github.com>
2023-06-28 20:00:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ app.listen(process.env.PORT || 3000, function() {
 });
 
 // FIRST PROJECT
-let image = document.getElementById("pfp");
+//let image = document.getElementById("pfp");
 
+/*
 image.addEventListener("click", function() {          window.location.replace("https://codepen.io/Angie-W/full/zYMNMPQ");
   //https://codepen.io/Angie-W/full/zYMNMPQ
 });
-
+*/
 
 // SECOND PROJECT
 /*
